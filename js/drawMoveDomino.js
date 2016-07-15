0ad3c3d5f0bb71d40de8e2f8ce8ca248daa56be4
Nozomi <yuma8980@gmail.com>
2016-07-15 06:41:48 +0000

    拚片可以轉來轉去啦啊啊啊啊啊啊啊啊啊#7

--- js/drawMoveDomino.js
+++ js/drawMoveDomino.js
@@ -1,8 +1,10 @@
 var canvasObj;//global variable才能被這串物件調用，如果單單只是宣告在主要物件中是沒辦法傳遞的，畢竟js並不是非常嚴謹的類別
-var delta,maxWidth,maxHeight;
+var delta,maxWidth,maxHeight,color;
 var x=200;
 var y=200;
-var drawMoveDomino = function(canvasIn,deltaIn,maxWidthIn,maxHeightIn){
+var minoInfo;
+var turnDirect=1,moveDirect=1;
+var drawMoveDomino = function(canvasIn,deltaIn,maxWidthIn,maxHeightIn,colorIn){
     // minoName:拚片名稱/代號
     // color:玩家顏色
     // canvasObj:畫布物件
@@ -12,26 +14,38 @@ var drawMoveDomino = function(canvasIn,deltaIn,maxWidthIn,maxHeightIn){
     // minoName,color,,chessStatus
     
     
-    // var total = 4;
-    // var minoTurn1 = [2,{x:0,y:0,w:1,h:2},{x:1,y:1,w:1,h:1}];
-    // var minoTurn2 = [2,{x:0,y:0,w:1,h:2},{x:1,y:0,w:1,h:1}];
-    // var minoTurn3 = [2,{x:0,y:0,w:1,h:1},{x:1,y:0,w:1,h:2}];
-    // var minoTurn4 = [2,{x:0,y:1,w:1,h:1},{x:1,y:0,w:1,h:2}];
-    // var minoInfo = [total,minoTurn1,minoTurn2,minoTurn3,minoTurn4];
+    var total = [4,2,2];
+    var minoTurn1 = [2,{x:0,y:0,w:1,h:2},{x:1,y:1,w:1,h:1}];
+    var minoTurn2 = [2,{x:0,y:0,w:1,h:2},{x:1,y:0,w:1,h:1}];
+    var minoTurn3 = [2,{x:0,y:0,w:1,h:1},{x:1,y:0,w:1,h:2}];
+    var minoTurn4 = [2,{x:0,y:1,w:1,h:1},{x:1,y:0,w:1,h:2}];
+    minoInfo = [total,minoTurn1,minoTurn2,minoTurn3,minoTurn4];
     
    
     delta=deltaIn;
     canvasObj=canvasIn;
     maxWidth=maxWidthIn;
     maxHeight=maxHeightIn;
+    color=colorIn;
     
     
 }
 drawMoveDomino.prototype ={
-     moveDomino : function(direction){
+     start :function(direction,turn){
          
-         var width  =60;
-         var height =20;
+         
+         
+    if(turn=="NONE")
+       moveDomino(direction);
+    else if(turn!="NONE")
+       turnDomino(turn);
+         
+         
+         
+     function moveDomino(direction){
+         
+         var width  =minoInfo[0][1]*delta;
+         var height =minoInfo[0][2]*delta;
          /*******************************************/
             if(direction=="Right")
             x+=delta;
@@ -51,10 +65,18 @@ drawMoveDomino.prototype ={
             else if(y+height>maxHeight)
             y-=delta;
          /**********有空要記得改這個鳥算法*********/
-            canvasObj.fillStyle="red";
-            canvasObj.fillRect(x,y,width,height);
-     },
-     turnDomino : function(turn){//還好turn不是保留字
+         for(var i=1;i<=minoInfo[moveDirect][0];i++){
+            canvasObj.fillStyle=color;
+            canvasObj.fillRect(
+                x+minoInfo[moveDirect][i].x*20,
+                y+minoInfo[moveDirect][i].y*20,
+                minoInfo[moveDirect][i].w*20,
+                minoInfo[moveDirect][i].h*20);
+         }
+     }
+     
+     
+     function turnDomino(turn){//還好turn不是保留字
             
             // context.fillStyle = "rgb(255,0,0)";
             // context.fillRect(paddleX, paddleY, paddleWidth, paddleHeight);
@@ -64,6 +86,26 @@ drawMoveDomino.prototype ={
             // canvasObj.fillStyle="grey";
             // canvasObj.fillRect(0,0,delta,delta);
             
+            if(turn=="turnClock")
+            turnDirect+=1;
+            else if(turn=="turnAntiClock")
+            turnDirect-=1;
+            
+            if(turnDirect<=0)
+            turnDirect+=4;
+            else if(turnDirect>=5)
+            turnDirect-=4;
+            
+            for(var i=1;i<=minoInfo[turnDirect][0];i++){
+            canvasObj.fillStyle=color;
+            canvasObj.fillRect(
+                x+minoInfo[turnDirect][i].x*20,
+                y+minoInfo[turnDirect][i].y*20,
+                minoInfo[turnDirect][i].w*20,
+                minoInfo[turnDirect][i].h*20);
+         }
+          moveDirect=turnDirect;  
         }
+     }
     
 }
